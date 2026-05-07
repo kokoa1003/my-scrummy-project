00b267080bafebdd82f5fb70ec8cc2e07748008c
kokoa1003 <mofumofucocoa3@gmail.com>
2026-05-07 14:40:16 +0900

    Implement brightness control using analog input

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <SchooMyUtilities.h>
+SchooMyUtilities scmUtils = SchooMyUtilities();
+
+float _sbeGetBrightness(int pinNumber, int res, float vol) {
+  pinMode(pinNumber, INPUT);
+  float cds_ad = analogRead(pinNumber);
+  float cds_v = cds_ad * vol / res;
+  float v_res = vol - cds_v;
+  if (v_res < 0.01) {
+    v_res = 0.01;
+  }
+  return 10000 * cds_v / v_res / 1000;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+}
+
+void loop() {
+  if ((_sbeGetBrightness(A1, 1023, 5) < 30)) {
+    digitalWrite(5, LOW);
+  }
+}
